53c06c68ab164345aed61a750e1ffc8817ba40bc
Chikavi1 <chikavi@hotmail.com>
2021-05-04 16:27:45 -0500

    arreglando cagadero en migraciones 2

--- migrations/20210130053905-add_name_to_usuarios.js
+++ migrations/20210130053905-add_name_to_usuarios.js
@@ -1,21 +1,21 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//   up: function(queryInterface, Sequelize) {
-//     // logic for transforming into the new state
-//     return queryInterface.addColumn(
-//       'Users',
-//       'name',
-//       Sequelize.STRING(60)
-//     );
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    // logic for transforming into the new state
+    // return queryInterface.addColumn(
+    //   'Users',
+    //   'name',
+    //   Sequelize.STRING(60)
+    // );
 
-//   },
+  },
 
-//   down: function(queryInterface, Sequelize) {
-//     // logic for reverting the changes
-//     return queryInterface.removeColumn(
-//       'Users',
-//       'name'
-//     );
-//   }
-// }
+  down: function(queryInterface, Sequelize) {
+    // logic for reverting the changes
+    // return queryInterface.removeColumn(
+    //   'Users',
+    //   'name'
+    // );
+  }
+}
