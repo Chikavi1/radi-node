cab161c6c898a4d11b223c664f55bad9803808ad
Sergio Gare <sergio.gare.sagg@gmail.com>
2021-06-11 20:53:42 -0500

    Correcciones Isolaciones y busqueda general

--- controllers/PublicControllers.js
+++ controllers/PublicControllers.js
@@ -12,7 +12,9 @@ exports.searchThings = async (req, res) => {
     result.pets = await Pets(DB, DataTypes).findAll({
         where: {
             "name": { [Op.like]: '%' + req.params.query + '%' }
-        }
+        },
+        attributes: ['name', 'photo'],
+        limit: 5
     }).catch(err => {
         res.status(503);
         res.json({ msg: err });
@@ -22,7 +24,8 @@ exports.searchThings = async (req, res) => {
     result.vets = await Vets(DB, DataTypes).findAll({
         where: {
             "name": { [Op.like]: '%' + req.params.query + '%' }
-        }
+        },
+        limit: 5
     }).catch(err => {
         res.status(503);
         res.json({ msg: err });
@@ -32,7 +35,8 @@ exports.searchThings = async (req, res) => {
     result.organizations = await Organizations(DB, DataTypes).findAll({
         where: {
             "name": { [Op.like]: '%' + req.params.query + '%' }
-        }
+        },
+        limit: 5
     }).catch(err => {
         res.status(503);
         res.json({ msg: err });
