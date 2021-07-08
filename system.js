8ed4eae834516d9566e7ae5e6bc406bdce402fa6
Chikavi1 <chikavi@hotmail.com>
2021-07-08 17:42:21 -0500

    agregue system para tener ambos una idea de los constantes de la plataforma

---
+++ system.js
@@ -0,0 +1,15 @@
+// 
+//      
+payment = [
+    0 = 'pago con efectivo',
+    1 = 'pago con tarjeta',
+    2 = 'pago con paypal'
+]
+
+pet_status = [
+    0 = 'muerto',
+    1 = 'adoptado',
+    2 = 'en adopcion',
+    3 = 'perdido'
+]
+
